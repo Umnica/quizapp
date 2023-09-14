06dbdb4adee565428dda783e2725fcf99b02e517
Kirill <vtorcevkirill@mail.ru>
2023-09-15 02:56:49 +0400

    теперь у логов есть файл

--- src/main/java/com/vtortsev/quizapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vtortsev/quizapp/exception/GlobalExceptionHandler.java
@@ -4,16 +4,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+@RestController
 //содержит методы-обработчики для исключений
-public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)//если возникает любое исключение
-    @PostMapping("/add")
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
-        //мы возвращаем ответ со статусом 500 и сообщением "An error occurred"
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
-        //добавить `@RequestMapping` или `@PostMapping`
+        // Логируем ошибку
+        logger.error("An error occurred: {}", ex.getMessage());
+
+        // Возвращаем статус 400 и сообщение об ошибке
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("An error occurred: " + ex.getMessage());
     }
 }
