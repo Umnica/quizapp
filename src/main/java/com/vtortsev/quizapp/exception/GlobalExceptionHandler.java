0fa974d51a35022f0e8000e9631f1ac9a4cf353e
Kirill <vtorcevkirill@mail.ru>
2023-09-15 23:36:40 +0400

    Добавлен параметр HttpServletRequest request в обработчики исключений, который позволяет получить информацию о текущем запросе, включая endpoint (request.getRequestURI()). Эта информация добавляется в лог перед текстом ошибки.

--- src/main/java/com/vtortsev/quizapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vtortsev/quizapp/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.vtortsev.quizapp.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -16,9 +14,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
+    public ResponseEntity<String> handleException(HttpServletRequest request, Exception ex) {
         // Логируем ошибку
-        logger.error("An error occurred: " + ex.getMessage());
+        logger.error("Error occurred for endpoint " + request.getRequestURI() + ": " + ex.getMessage());
 
         // Возвращаем статус 400 и сообщение об ошибке
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("An error occurred: " + ex.getMessage());
@@ -26,9 +24,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // Обработчик для исключений типа Error
     @ExceptionHandler(Error.class)
-    public ResponseEntity<String> handleError(Error error) {
+    public ResponseEntity<String> handleError(HttpServletRequest request, Error error) {
         // Логируем ошибку
-        logger.error("An error occurred: " + error.getMessage());
+        logger.error("Error occurred for endpoint " + request.getRequestURI() + ": " + error.getMessage());
 
         // Возвращаем статус 400 и сообщение об ошибке
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("An error occurred: " + error.getMessage());
