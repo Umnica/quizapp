8992f1946e953e5936f9b3792f76337c4af19855
Kirill <vtorcevkirill@mail.ru>
2023-09-16 02:27:51 +0400

    toFullDto

--- src/main/java/com/vtortsev/quizapp/dto/mapper/QuestionMapper.java
+++ src/main/java/com/vtortsev/quizapp/dto/mapper/QuestionMapper.java
@@ -1,5 +1,6 @@
 package com.vtortsev.quizapp.dto.mapper;
 
+import com.vtortsev.quizapp.dto.FullQuestionDto;
 import com.vtortsev.quizapp.dto.QuestionDto;
 import com.vtortsev.quizapp.entities.Question;
 import org.mapstruct.Mapper;
@@ -16,4 +17,11 @@ public interface QuestionMapper {
     @Mapping(source = "level", target = "level")
     @Mapping(source = "questionText", target = "questionText")
     Question toEntity(QuestionDto questionDto);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "level", target = "level")
+    @Mapping(source = "questionText", target = "questionText")
+    @Mapping(source = "answers", target = "answers")
+    @Mapping(source = "categories", target = "categories")
+    FullQuestionDto toFullDto(Question question);
 }
