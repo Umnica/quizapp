9e86e7f4e0f2cd5fcfceacd9f63ee4c5f140db67
Kirill <vtorcevkirill@mail.ru>
2023-09-16 03:50:09 +0400

    добавил создание Question без валидации

---
+++ src/main/java/com/vtortsev/quizapp/dto/CreateQuestionDto.java
@@ -0,0 +1,11 @@
+package com.vtortsev.quizapp.dto;
+
+import lombok.Data;
+import org.intellij.lang.annotations.Pattern;
+
+@Data
+public class CreateQuestionDto {
+    private String level;
+    //@Pattern(regexp = "^[a-zA-Zа-яА-Я0-9\\-.,!?]+$")
+    private String questionText;
+}
