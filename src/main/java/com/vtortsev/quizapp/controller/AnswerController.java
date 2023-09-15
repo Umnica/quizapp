689ba671ead9da5173a999a181618585f2b65db7
Kirill <vtorcevkirill@mail.ru>
2023-09-15 22:09:14 +0400

    добавил mapper

--- src/main/java/com/vtortsev/quizapp/controller/AnswerController.java
+++ src/main/java/com/vtortsev/quizapp/controller/AnswerController.java
@@ -1,6 +1,8 @@
 package com.vtortsev.quizapp.controller;
 
+import com.vtortsev.quizapp.dto.AnswerDto;
 import com.vtortsev.quizapp.entities.Answer;
+import com.vtortsev.quizapp.dto.mapper.AnswerMapper;
 import com.vtortsev.quizapp.service.AnswerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +12,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
 @RequestMapping("/answers")
 public class AnswerController {
     private final AnswerService answerService;
+    private final AnswerMapper answerMapper;
     @Autowired
+    public AnswerController(AnswerService answerService, AnswerMapper answerMapper) {
+        this.answerService = answerService;
+        this.answerMapper = answerMapper;
+    }
 
-    public AnswerController(AnswerService answerService) { this.answerService = answerService; }
 
+    //@GetMapping
+    //@ResponseBody
+    //public List<Answer> getAllAnswer(){ return answerService.getAllAnswer(); }
 
     @GetMapping
     @ResponseBody
-    public List<Answer> getAllAnswer(){ return answerService.getAllAnswer(); }
+    public List<AnswerDto> getAllAnswer() {
+        List<Answer> answers = answerService.getAllAnswer();
+        return answers.stream()
+                .map(answerMapper::toDto).
+                collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/com/vtortsev/quizapp/dto/AnswerDto.java
@@ -0,0 +1,10 @@
+package com.vtortsev.quizapp.dto;
+
+import lombok.Data;
+
+@Data
+public class AnswerDto {
+    private Integer id;
+    private String answerText;
+    private Integer questionId;
+}
