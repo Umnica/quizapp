d1a49dc49777e9f7051fbb02f89d2aa2846bbd2e
Kirill <vtorcevkirill@mail.ru>
2023-09-20 21:02:03 +0400

    public Answer createAnswer(Answer answerIn)

--- src/main/java/com/vtortsev/quizapp/service/AnswerService.java
+++ src/main/java/com/vtortsev/quizapp/service/AnswerService.java
@@ -36,13 +36,14 @@ public class AnswerService {
         answer.setAnswerText(createAnswerDto.getAnswerText());
         return answerDao.save(answer);
     }
-    public Answer createAnswer(Answer createAnswerDto) {
-        if (!Valid.isValidText(createAnswerDto.getAnswerText()))
+
+    public Answer createAnswer(Answer answerIn) {
+        if (!Valid.isValidText(answerIn.getAnswerText()))
             throw new IllegalArgumentException("Invalid answer text");
 
-        Answer answer = new Answer();
-        answer.setAnswerText(createAnswerDto.getAnswerText());
-        answer.setQuestion(createAnswerDto.getQuestion());
-        return answerDao.save(answer);
+        Answer answerOut = new Answer();
+        answerOut.setAnswerText(answerIn.getAnswerText());
+        answerOut.setQuestion(answerIn.getQuestion());
+        return answerDao.save(answerOut);
     }
 }
