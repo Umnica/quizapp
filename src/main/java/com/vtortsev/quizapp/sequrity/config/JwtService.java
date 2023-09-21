805baa4d1a1e8f58b6d8beec78d8975d974135ac
Kirill <vtorcevkirill@mail.ru>
2023-09-22 02:32:53 +0400

    +funs для валидации, генерации токена

--- src/main/java/com/vtortsev/quizapp/sequrity/config/JwtService.java
+++ src/main/java/com/vtortsev/quizapp/sequrity/config/JwtService.java
@@ -2,11 +2,16 @@ package com.vtortsev.quizapp.sequrity.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -17,11 +22,43 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
+    // Этот метод обобщенный и принимает тип T
+    // claimsTFunction: функция, которая принимает объект Claims (утверждения из токена) и возвращает значение типа T
+    // extractAllClaims(token): извлекает все утверждения из токена
+    // claimsTFunction.apply(claims): применяет переданную функцию claimsTFunction к объекту Claims (утверждениям) для извлечения значения
     public <T> T extractClaim(String token, Function<Claims, T> claimsTFunction) {
         final Claims claims = extractAllClaims(token);
         return claimsTFunction.apply(claims);
     }
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
 
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()*1000*60*24))
+                .signWith(getSignInkey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
     // претензии
     private Claims extractAllClaims(String token) {
         return Jwts
