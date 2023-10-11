37cc04b74750f7e78b3f4a0bbcab5275a8d13aa5
Kirill <vtorcevkirill@mail.ru>
2023-10-11 14:26:14 +0400

    не понимаю почему не работает

--- src/main/java/com/vtortsev/quizapp/QuizappApplication.java
+++ src/main/java/com/vtortsev/quizapp/QuizappApplication.java
@@ -2,13 +2,10 @@ package com.vtortsev.quizapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class QuizappApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) {;
         SpringApplication.run(QuizappApplication.class, args);
     }
 
